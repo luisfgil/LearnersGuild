9393d67927567022bfe8c3c44e3e03207d41e423
luis <luis@gigbelt.com>
2017-03-25 02:11:40 -0400

    Adding files for Color Picker

---
+++ script.js
@@ -0,0 +1,11 @@
+function setColor(color) {
+  var body = document.querySelector("body");
+
+  body.className = color;
+}
+
+function setTextColor(color) {
+  var text = document.getElementById("text");
+
+  text.className = color;
+}
